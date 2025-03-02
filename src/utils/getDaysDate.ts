42f83623df4436b22cd2059b4f096ec92edb94da
Leandro <leandrogomesdeoliveira2@gmail.com>
2025-03-02 13:43:17 -0300

    fix getDaysDate

--- src/utils/getDaysDate.ts
+++ src/utils/getDaysDate.ts
@@ -3,31 +3,52 @@ const day: Date = new Date()
 export const arrayDay: string[] = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb']
 export const arrayDate: number[] = []
 
-export const getDaysDate = (set: React.Dispatch<React.SetStateAction<number[]>>) => {
+// export const getDaysDate = (set: React.Dispatch<React.SetStateAction<number[]>>) => {
     
-    const today = new Date(); 
-    const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
-    const firstDayOfWeek = firstDayOfMonth.getDate() - firstDayOfMonth.getDay();
-    const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
+//     const today = new Date(); 
+//     const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+//     const firstDayOfWeek = firstDayOfMonth.getDate() - firstDayOfMonth.getDay();
+//     const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
 
-    const days = [];
+//     const days = [];
 
-    for (let i = 0; i < arrayDay.length; i++) {
-        let calculatedDay = firstDayOfWeek + i;
+//     for (let i = 0; i < arrayDay.length; i++) {
+//         let calculatedDay = firstDayOfWeek + i;
         
-        if (calculatedDay <= 0){
-            const lastDayOfPreviousMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate()
-            calculatedDay = lastDayOfPreviousMonth + calculatedDay;
-        }
+//         if (calculatedDay <= 0){
+//             const lastDayOfPreviousMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate()
+//             calculatedDay = lastDayOfPreviousMonth + calculatedDay;
+//         }
 
-        if (calculatedDay > lastDayOfMonth) {
-            calculatedDay -= lastDayOfMonth;
-        }
+//         if (calculatedDay > lastDayOfMonth) {
+//             calculatedDay -= lastDayOfMonth;
+//         }
 
-        days.push(calculatedDay);
-    }
-    set(days);
+//         days.push(calculatedDay);
+//     }
+//     set(days);
     
+// }
+
+export const getDaysDate = (set: React.Dispatch<React.SetStateAction<number[]>>) => {
+    const today = new Date("December 1, 1995")
+    console.log(today.getDay());
+    
+
+
+    const days: number[] = []
+    
+    for (let i = 0; i < arrayDay.length; i++) {
+        days[today.getDay() + i] = today.getDate() + i
+        // if(today.getDay() >= 0) {
+        //     days[today.getDay() + i] = today.getDate() + i
+        // }
+        if(today.getDay() > 0) {
+            days[today.getDay() - i] = today.getDate() - i
+        }
+    }
+
+    set(days);
 }
 
 
