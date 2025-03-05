4ce0ee6e7f83afd8c19207ee04bcb98f67dc4579
Leandro <leandrogomesdeoliveira2@gmail.com>
2025-03-05 17:12:57 -0300

    add alarm sound, clock tic and alarm effect

--- src/sections/time/Timer.tsx
+++ src/sections/time/Timer.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
 import { handleScroll, handleInfiniteScroll, handleScrollPlay } from '../../utils/timer'
+import ticSound from '../../assets/audio/tic.mp3';
+import alarmSound from '../../assets/audio/alarm.mp3';
 import './timer.scss'
 
 const tensValues: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -11,11 +13,15 @@ function Timer() {
     const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
 
     
-    const audio: HTMLAudioElement = new Audio('');
+    const alarmAudio: HTMLAudioElement = new Audio(alarmSound);
+    const timerAudio: HTMLAudioElement = new Audio(ticSound);
+    timerAudio.volume = 0.4
 
+    const containerRef = useRef<HTMLDivElement>(null)
     const tensRef = useRef<HTMLInputElement>(null)
     const unitsRef = useRef<HTMLInputElement>(null)
     const intervalRef = useRef<number | null>(null);
+    const alarmRef = useRef<number | null>(null);
 
     useEffect(() => {
         const tensElement = tensRef.current
@@ -34,8 +40,8 @@ function Timer() {
             handleInfiniteScroll(unitsElement)
     
             isScrolling = setTimeout(() => {
-                handleScroll(tensElement, setTensState);
-                handleScroll(unitsElement, setUnitsState);   
+                handleScroll(containerRef, tensElement, setTensState, alarmRef);
+                handleScroll(containerRef, unitsElement, setUnitsState, alarmRef);   
                 
             }, 500);
         };
@@ -51,11 +57,12 @@ function Timer() {
     }, [tensState, unitsState]);
 
     const handleTimerButtonClick = () => {
-        handleScrollPlay(tensRef.current, unitsRef.current, tensState, unitsState, intervalRef, setIsTimerRunning, audio)
+        handleScrollPlay(containerRef, tensRef.current, unitsRef.current, tensState, unitsState, intervalRef, setIsTimerRunning, alarmAudio, timerAudio, alarmRef)
     };
 
     return (
-        <div className="timerContainer">
+        <div className="timerContainer" ref={containerRef}>
+            <div className='alarm'></div>
             <div className='overflowContainer' ref={tensRef}>
                 {tensValues.map((value, id) => (
                     <div key={id} className='pContainer' >
--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -1,5 +1,14 @@
-export const handleScroll = (element:HTMLInputElement, set:React.Dispatch<React.SetStateAction<number>>) => {
+export const handleScroll = (
+    container: React.MutableRefObject<HTMLDivElement | null>,
+    element:HTMLInputElement, 
+    set:React.Dispatch<React.SetStateAction<number>>, 
+    alarmRef: React.MutableRefObject<number | null>) => {
     
+    if(alarmRef.current !== null) clearInterval(alarmRef.current)
+    if (container.current) { 
+        container.current.classList.remove('alarm')
+    }
+
     const offset = 35;
     const scrollTop = element.scrollTop
 
@@ -20,14 +29,14 @@ export const handleScroll = (element:HTMLInputElement, set:React.Dispatch<React.
             const numberTake: number = parseInt(`${fDiv?.querySelector("p")?.textContent || 0}`)
         
             selectedNumber = numberTake
+
+            
         }
     })
 
     if (scrollTop <= offset) {
         element.scrollTo({ top: 0, behavior: "smooth" });
         selectedNumber = 0;
-
-        console.log('opa');
         
     }
 
@@ -63,17 +72,24 @@ export const handleInfiniteScroll = (element: HTMLInputElement) => {
 
 
 export const handleScrollPlay = (
+    container: React.MutableRefObject<HTMLDivElement | null>,
     tensElement: HTMLInputElement | null, 
     unitElement: HTMLInputElement | null, 
-    secondsTens: number, secondsDigit : number, 
+    secondsTens: number, 
+    secondsDigit : number, 
     intervalRef: React.MutableRefObject<number | null>,
-    setIsTimerRunning: React.Dispatch<React.SetStateAction<boolean>>, audios: HTMLAudioElement
+    setIsTimerRunning: React.Dispatch<React.SetStateAction<boolean>>, 
+    audios: HTMLAudioElement, 
+    tic: HTMLAudioElement,
+    alarmRef: React.MutableRefObject<number | null>
 ) => {
 
     if (!tensElement || !unitElement) return
 
     let tens = secondsTens;
     let unit = secondsDigit;
+    let count: number = 0
+    const maxPlays = 3
 
     if (intervalRef.current !== null) {
         clearInterval(intervalRef.current) 
@@ -81,19 +97,37 @@ export const handleScrollPlay = (
         setIsTimerRunning(false)
         return;
     }
+
     if (tens === 0 && unit === 0) return;
     
     setIsTimerRunning(true)
 
     const updateScroll = () => {
-        console.log(tens, unit);
         
         if (tens === 0 && unit === 0) {
-            if(intervalRef.current !== null) {
+            if (intervalRef.current !== null) {
                 clearInterval(intervalRef.current) 
                 intervalRef.current = null;
                 setIsTimerRunning(false)
+
                 audios.play()
+                if (container.current) {
+                    container.current.classList.add('alarm')
+                }
+
+                alarmRef.current = setInterval(() => {
+                    audios.play()
+
+                    count++
+                    
+                    if (count >= maxPlays) { 
+                        if(alarmRef.current !== null) clearInterval(alarmRef.current)
+                        if (container.current) {
+                            container.current.classList.remove('alarm')
+                        }
+                    }
+
+                }, 3000)
                 return;
             }
 
@@ -102,6 +136,9 @@ export const handleScrollPlay = (
             unitElement.scrollTo({top: unitElement.scrollTop - 81, behavior: 'smooth'})
 
             unit--
+
+            tic.pause()
+            tic.play()
             return
         }
         if (unit === 0 && tens > 0) {
@@ -110,6 +147,9 @@ export const handleScrollPlay = (
 
             tens--
             unit = 9
+
+            tic.pause()
+            tic.play()
             return
         }
     }
